c842254c3194ef41738db3fa031b0103b543db14
Daniel King <dking@niche.com>
2021-04-29 18:55:44 -0400

    Assignment 1
    Look at that!

--- Assignment 1.sql
+++ Assignment 1.sql
@@ -4,3 +4,24 @@ group by 1
 order by 2 desc
 
 --- Question 1: Bridgeport
+
+select metro_city, max(population) as biggestsweets
+from census_metro_data
+group by 1
+order by 2 desc
+
+--- Question 2: Houston
+
+select state, count(distinct metro_city)
+from census_metro_data
+group by 1
+order by 2 desc
+
+--- Question 3: CA/10
+
+select metro_city, sum (population_age_70_74+population_age_75_79)/sum(population)::numeric
+from census_metro_data
+group by 1
+order by 2 desc
+
+--- Question 4: Sarasota
